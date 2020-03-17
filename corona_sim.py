8f31dfd9f75f8e55ec0e8aca211583b8e109fb4e
Pranav <pranav.ppanch@gmail.com>
2020-03-17 19:59:37 +0530

    Death and Recovery

--- corona_sim.py
+++ corona_sim.py
@@ -21,17 +21,18 @@ def velocity_perturbation(xx, maxvel=5):
 
 # Let's set the parameters
 # ------------------------------
-N = 500
+N = 1000
 box = 10.0  # Size of our box
-tmax = 2.0  # Final time
-nsteps = 3000
+tmax = 2  # Final time
+nsteps = 1000
 npart = 1000
 nvel = 1
 D = 2
 
 xx = np.random.random(size=(N, D)) * box
-vv = velocity_perturbation(xx, 5)
+vv = velocity_perturbation(xx, 15)
 corona = np.zeros(len(xx)).astype("bool")
+death = np.zeros(len(xx)).astype("bool")
 
 for _ in range(10):
     corona[np.random.randint(0, high=len(corona))] = True
@@ -43,10 +44,11 @@ fig = plt.figure(figsize=(8, 8))
 plt.axis("on")
 time = 0
 ims = []
-color = np.full(len(xx), "b")
 
 # DATA for plots
 infected = []
+uninfected = []
+dead = []
 
 
 # Main loop
@@ -57,20 +59,41 @@ for _ in tqdm.tqdm(np.arange(0, nsteps)):
     xx = periodic(xx + delta_t / 2 * vv, box=box)
     time += delta_t
 
-    for inf in xx[corona]:
-        corona = np.logical_or(corona, (np.linalg.norm(xx - inf, axis=1) < 1e-1))
+    if np.sum(corona) < N / 2:
+        for inf in xx[corona]:
+            corona = np.logical_or(corona, (np.linalg.norm(xx - inf, axis=1) < 2e-1))
+    else:
+        for inf in xx[~corona]:
+            corona = np.logical_or(corona, (np.linalg.norm(xx - inf, axis=1) < 2e-1))
 
+    corona = np.logical_and(np.random.random(len(xx)) > 0.10, corona)
+    death = np.logical_or(
+        np.logical_and(np.random.random(len(xx)) < 0.004, corona), death,
+    )
+    corona[death] = False
+    vv[death] = np.array([0, 0])
     # -------------------------------
-    color[corona] = "r"
-    # im = plt.scatter(xx[:, 0], xx[:, 1], animated=True, color=color)
-    # ims.append([im])
-    infected.append(np.sum(corona))
 
-# ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
-# ani.save("phase_space_1.mp4")
-plt.plot(np.linspace(0, tmax, nsteps), infected)
-plt.xlabel("time")
-plt.ylabel("infected")
-plt.yscale("log")
+    color = np.full(len(xx), "b")
+    color[corona] = "r"
+    color[death] = "k"
+    im = plt.scatter(xx[:, 0], xx[:, 1], animated=True, color=color, marker=".")
+    ims.append([im])
+    infected.append(np.sum(corona))
+    uninfected.append(np.sum(np.logical_and(~corona, ~death)))
+    dead.append(np.sum(death))
+
+ani = animation.ArtistAnimation(fig, ims, interval=50, blit=True, repeat_delay=1000)
+ani.save("corona_sim.mp4")
+
+plt.figure()
+plt.plot(np.linspace(0, tmax, nsteps), infected, color="r", label="Infected")
+plt.plot(np.linspace(0, tmax, nsteps), uninfected, color="b", label="Un-infected")
+plt.plot(np.linspace(0, tmax, nsteps), dead, color="k", label="Dead")
+plt.xlabel("Time")
+plt.ylabel("Number")
+plt.legend()
+plt.title(f"N:{N} d:{D}")
+plt.axhline(y=N, color="y", linestyle="-")
 plt.savefig("infected.png", format="png")
 plt.show()
